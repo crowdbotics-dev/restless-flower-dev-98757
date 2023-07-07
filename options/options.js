09e45b367fc53e3a2196dccd6ac7a2b55ebc38da
Crowdbotics <engineering@crowdbotics.com>
2023-07-07 12:37:53 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "restless-flower-dev-98757",
+  url: "https://restless-flower-dev-98757.botics.co",
+  api: "https://restless-flower-dev-98757.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
